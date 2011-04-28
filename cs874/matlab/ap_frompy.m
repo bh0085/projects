5492b1597fad396368218203cdb21c57eccfccae
Benjamin Holmes <bh0085@gliese.csail.mit.edu>
2011-04-28 17:32:28 -0400

    messageless push on Thu Apr 28 17:32:28 EDT 2011

--- cs874/matlab/ap_frompy.m
+++ cs874/matlab/ap_frompy.m
@@ -1,18 +1,15 @@
 function ap_frompy(mat_inp, mat_out)
 data = open(mat_inp)
 
-out_struct = struct()
-out_struct.ss = data.self_similarity
-out_struct.metric = data.metric
-out_struct.inds = []
-save(mat_out, 'out_struct')
-
 sims = data.similarities
 ss   = data.self_similarity
 inds = ap(sims, ss)
-out_struct.inds = ap(sims, ss)
+
+
 out_struct = struct()
+out_struct.inds = inds
 out_struct.ss = ss
 out_struct.metric = data.metric
+
 save(mat_out, 'out_struct')
 exit
