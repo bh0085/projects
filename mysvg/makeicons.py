0518037dbe2d0a824ac0e44d0bc5083f62cc146e
ben <bh0085@gmail.com>
2013-02-05 11:45:42 -0500

    messageless push on Tue Feb  5 11:45:42 EST 2013

--- mysvg/makeicons.py
+++ mysvg/makeicons.py
@@ -5,7 +5,7 @@ import subprocess as spc
 colors = ['white','lightgray', 'black', 'darkgray']
 sizes = [128, 64, 32]
 
-def main():
+def main(make_imgs = False):
     print [os.path.splitext(s) for s in os.listdir(os.getcwd())]
     svgs = [ s for s in os.listdir(os.getcwd()) if os.path.splitext(s)[-1] == '.svg']
     svgs = [ s for s in svgs if not 'color=' in s]
@@ -32,15 +32,17 @@ def main():
             out_open.write(out)
             out_open.close()
             
-            for r in sizes:
-                size_file = color_file + '_size={0}'.format(r)
-                size_outpath = os.path.join(os.getcwd(),size_file + '.png')
+            if make_imgs:
+                for r in sizes:
+                    size_file = color_file + '_size={0}'.format(r)
+                    size_outpath = os.path.join(os.getcwd(),size_file + '.png')
                                             
-                cmd = 'rsvg -h {0} -w {0} {1} {2}'\
-                    .format(r, color_outpath, size_outpath)
-                spc.Popen(cmd,shell = True).communicate()
+                    cmd = 'rsvg -h {0} -w {0} {1} {2}'\
+                        .format(r, color_outpath, size_outpath)
+                    spc.Popen(cmd,shell = True).communicate()
             
 
     
 if __name__ == "__main__":
-    main()
+    imgs = False
+    main(make_imgs = imgs)
